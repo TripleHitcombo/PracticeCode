8fe0f8d3e2a851ae823c90d42e63d0b872c5043a
Carl Jones <161535663+TripleHitcombo@users.noreply.github.com>
2025-08-15 11:51:22 +0100

    Number Guesser Game
    User inputs a numbers, a random number is generated between 0 and the inputted number. The player must guess what that number is. A hint is given as to whether they are above or below the number, then the number of attempts is outputted on success.

---
+++ Number Guesser.py
@@ -0,0 +1,37 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Numbers must be above 0!")
+        quit()
+else:
+    print("Please type a number!")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Guess a number: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number!")
+        continue
+
+    if user_guess == random_number:
+        print("You guessed right!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses!")
+
+        
